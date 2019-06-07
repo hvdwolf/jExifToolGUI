2f29f3394228fd714c4400e5c5ed4e808ddc75e3
hvdwolf <hvdwolf@gmail.com>
2019-06-07 09:14:42 +0200

    .. and remove tablemodel again

--- src/main/java/org/hvdw/jexiftoolgui/mainScreen.java
+++ src/main/java/org/hvdw/jexiftoolgui/mainScreen.java
@@ -26,7 +26,6 @@ import javax.swing.*;
 import javax.swing.event.ListSelectionEvent;
 import javax.swing.event.ListSelectionListener;
 import javax.swing.event.MenuListener;
-import javax.swing.table.DefaultTableModel;
 import java.awt.*;
 import java.awt.event.*;
 import java.io.File;
