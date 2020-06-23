fe0b9ba14876f3e2788f5aa5011e355d25fe9f7a
hvdwolf <hvdwolf@gmail.com>
2020-06-23 19:56:29 +0200

    new windows packagig script

--- packaging/windows/create_exe_and_zip.sh
+++ packaging/windows/create_exe_and_zip.sh
@@ -1,16 +1,31 @@
 #!/bin/bash
 
-# Download launch4j from http://launch4j.sourceforge.net/
+# Script to make a windows 64bit executable linked to local V11 jre
+# version 0.2, 2020-06-23, hvdw
+
+# Requirements:
+# launch4j from http://launch4j.sourceforge.net/
+#     launch4j uses java v8, 9 or 10. Not 11 or above
+# jre V11: download from https://adoptopenjdk.net/releases.html.
+#      Untar it and rename resulting folder to jre (to keep it simple over the versions)
 
 launch4jPATH="../../../launch4j/launch4j"
-Version="0.1"
+Version="1.00"
+exe_zip_path="jExifToolGUI-$Version-x86_64"
+JRE="../../../java/JREs/windows-jre"
 
-$launch4jPATH ./jexiftoolgui.xml
+cp ../../jExifToolGUI.jar .
 
-mkdir "jExifToolGUI-$Version"
-mv *.exe "jExifToolGUI-$Version"
-cp ../../src/org/hvdw/jexiftoolgui/resources/COPYING "jExifToolGUI-$Version"
+# use jexiftoolgui-jre to link the exe against the local jre
+$launch4jPATH ./jexiftoolgui-jre.xml
 
-zip -9 -r "jExifToolGUI-$Version.zip" "jExifToolGUI-$Version"
-rm -rf "jExifToolGUI-$Version"
+rm -rf ${exe_zip_path}
+mkdir ${exe_zip_path}
+mv *.exe ${exe_zip_path}
+cp ../../LICENSE ${exe_zip_path}
+cp -a "${JRE}" ${exe_zip_path}/jre
 
+rm "${exe_zip_path}.zip"
+zip -r9 "${exe_zip_path}.zip" "${exe_zip_path}"
+
+rm -rf ${exe_zip_path}
