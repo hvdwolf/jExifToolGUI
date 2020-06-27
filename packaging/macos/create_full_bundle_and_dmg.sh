53e32e0b5fff5e3ae5bd9af7faad8a8e2237fb5f
hvdwolf <hvdwolf@gmail.com>
2020-06-27 16:39:37 +0200

    add macos bundle script plus base app

--- packaging/macos/create_full_bundle_and_dmg.sh
+++ packaging/macos/create_full_bundle_and_dmg.sh
@@ -1,31 +1,41 @@
 #!/bin/bash
 
-# Download jar2app from https://github.com/Jorl17/jar2app
-# Download the latest JRE for Mac OS/X if you want to bundle java with the app (-r ....jdk/jre)
-
 # Some variables
-jar2appPATH="../../../jar2app/jar2app"
-Version="1"
+BaseApp="jExifToolGUI.app.base"
+AppName="jExifToolGUI"
+App="jExifToolGUI.app"
 
-# Do initial cleanup
-rm -rf jExifToolGUI.app tmp *.dmg
+if [ "$1" = "" ]
+then
+        printf "\n\nYou have to provide the version\n\n"
+        exit
+fi
+
+Version="$1"
+
+printf "Do initial cleanup\n\n"
+rm -rvf jExifToolGUI.app tmp *.dmg
 # Create the app bundle
-# full bundle with jdk
-$jar2appPATH ../../build/libs/jExifToolGUI.jar jExifToolGUI -n jExifToolGUI -i ./appIcon.icns -r jdk1.8.0_212.jdk.zip
-# default bundle without jdk
-#$jar2appPATH ../../build/libs/ExifToolGUI.jar jExifToolGUI -n jExifToolGUI -i ./appIcon.icns
+# full bundle with jre
+printf "Create the bundle from the base bundle\n\n"
+cp -a $BaseApp $App
 
-# Create the dmg
+printf "Now copy the jre and the jar into the app\n\n" 
+cp -a jre $App/Contents/MacOS
+cp ../../jExifToolGUI.jar $App/Contents/MacOS/
+
+printf "Create the 125MB (full) dmg\n\n"
 mkdir -p tmp/dmg
-# 1MB dmg
-dd if=/dev/zero of=tmp/jExifToolGUI.dmg bs=1M count=360
+# 125MB dmg
+dd if=/dev/zero of=tmp/jExifToolGUI.dmg bs=1M count=125
 
-mkfs.hfsplus -v "jExifToolGUI $Version" tmp/jExifToolGUI.dmg
+mkfs.hfsplus -v "jExifToolGUI-x86_64 $Version" tmp/jExifToolGUI.dmg
 
 sudo mount -o loop tmp/jExifToolGUI.dmg tmp/dmg
-sudo mv  jExifToolGUI.app tmp/dmg/
-sudo cp ../../src/org/hvdw/jexiftoolgui/resources/COPYING tmp/dmg/
+sudo cp -a  jExifToolGUI.app tmp/dmg/
+#sudo mv  jExifToolGUI.app tmp/dmg/
+sudo cp ../../LICENSE tmp/dmg/
 
 sudo umount tmp/dmg
-mv tmp/jExifToolGUI.dmg ./"jExifToolGUI-full-$Version.dmg"
+mv tmp/jExifToolGUI.dmg ./"jExifToolGUI-x86_64-full-$Version.dmg"
 rm -rf tmp 
