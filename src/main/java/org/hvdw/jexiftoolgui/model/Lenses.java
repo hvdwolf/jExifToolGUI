4200c1d6d2ecfc8b619c4ac4f0f14b1d0dc2bae4
hvdwolf <hvdwolf@gmail.com>
2023-03-21 22:23:27 +0100

    rewrite favorites and lenses and Exiftoolreference; remove more sqlite II

--- src/main/java/org/hvdw/jexiftoolgui/model/Lenses.java
+++ src/main/java/org/hvdw/jexiftoolgui/model/Lenses.java
@@ -1,12 +1,15 @@
 package org.hvdw.jexiftoolgui.model;
 
+import org.hvdw.jexiftoolgui.MyVariables;
 import org.hvdw.jexiftoolgui.controllers.SQLiteJDBC;
 import org.hvdw.jexiftoolgui.facades.SystemPropertyFacade;
 import org.slf4j.LoggerFactory;
 
 import javax.swing.*;
 import javax.swing.table.DefaultTableModel;
-import java.util.Arrays;
+import java.io.File;
+import java.util.ArrayList;
+import java.util.List;
 import java.util.ResourceBundle;
 
 import static org.hvdw.jexiftoolgui.facades.SystemPropertyFacade.SystemPropertyKey.LINE_SEPARATOR;
@@ -14,35 +17,36 @@ import static org.hvdw.jexiftoolgui.facades.SystemPropertyFacade.SystemPropertyK
 public class Lenses {
     private final static ch.qos.logback.classic.Logger logger = (ch.qos.logback.classic.Logger) LoggerFactory.getLogger(Lenses.class);
 
-    public static String loadlensnames() {
-        String sql = "select lens_name,lens_description from myLenses order by lens_Name";
-        String lensnames = SQLiteJDBC.generalQuery(sql, "disk");
-        //lblLensNames.setText(String.format(ProgramTexts.HTML, 300, lensnames.replace("\n", "<br>")));
-        return lensnames;
+    public static List<File> loadlensnames() {
+
+        File lensFolder = new File(MyVariables.getlensFolder());
+        File[] listOfFiles = lensFolder.listFiles();
+        List<File> listOfCheckedFileNames = new ArrayList<File>();
+
+        for (File file : listOfFiles) {
+            if (file.isFile()) {
+                //System.out.println(file.getName());
+                listOfCheckedFileNames.add(file);
+            }
+        }
+        return listOfCheckedFileNames;
 
     }
-    public static void displaylensnames(String lensnames, JTable lensnametable) {
+    public static void displaylensnames(List<File> lensnames, JTable lensnametable) {
         DefaultTableModel model = (DefaultTableModel) lensnametable.getModel();
-        model.setColumnIdentifiers(new String[]{ResourceBundle.getBundle("translations/program_strings").getString("sellens.name"), ResourceBundle.getBundle("translations/program_strings").getString("sellens.descr")});
-        lensnametable.getColumnModel().getColumn(0).setPreferredWidth(150);
-        lensnametable.getColumnModel().getColumn(1).setPreferredWidth(300);
+        model.setColumnIdentifiers(new String[]{ResourceBundle.getBundle("translations/program_strings").getString("sellens.name")});
+        lensnametable.getColumnModel().getColumn(0).setPreferredWidth(250);
         model.setRowCount(0);
 
         Object[] row = new Object[1];
 
-        if (lensnames.length() > 0) {
-            String[] lines = lensnames.split(SystemPropertyFacade.getPropertyByKey(LINE_SEPARATOR));
-            logger.debug("String[] lines {}", Arrays.toString(lines));
+        if (lensnames.size() > 0) {
+            //String[] lines = lensnames.split(SystemPropertyFacade.getPropertyByKey(LINE_SEPARATOR));
+            //logger.debug("String[] lines {}", Arrays.toString(lines));
 
-            for (String line : lines) {
-                logger.debug("line {}", line);
-                String[] cells = line.split("\\t");
-                logger.debug("number of elements after splitted line {}", cells.length);
-                if (cells.length > 1) {
-                    model.addRow(new Object[]{cells[0], cells[1]});
-                } else {
-                    model.addRow(new Object[]{line, ""});
-                }
+            for (File lensname : lensnames) {
+                logger.debug("lensname {}", lensname.getName());
+                model.addRow(new Object[]{lensname.getName()});
             }
         }
     }
